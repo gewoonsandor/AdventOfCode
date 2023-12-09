87d41ba48d55bb393ed93a09e37564a3dbf30309
gewoonsandor <s.v.wieringen1@student.tue.nl>
2023-12-09 09:40:39 +0100

    Zoek de verschillen tussen deel 1 en deel 2

--- 9/solution2.py
+++ 9/solution2.py
@@ -5,21 +5,13 @@ with open("input.txt", "r") as f:
 lines = [[int(x) for x in line.strip().split(" ")] for line in lines]
 lines = [list(reversed(x)) for x in lines]
 
-def next_prediction(line):
-    return [b - a for a, b in pairwise(line)]
-
 output = 0
-
-def check_predictions(predictions):
-    for item in predictions:
-        if item != 0: return False
-    return True
 
 for line in lines:
     preditcion = []
     preditcion.append(line)
-    while(not check_predictions(preditcion[-1])):
-        preditcion.append(next_prediction(preditcion[-1]))
+    while(not all(x == 0 for x in preditcion[-1])):
+        preditcion.append([b - a for a, b in pairwise(preditcion[-1])])
     outcome = preditcion[-2][-1]
     preditcion = list(reversed(preditcion))
     preditcion.pop(0)
