b483f3776f9b86d6c404f2ede12abf50dffa71de
gewoonsandor <s.v.wieringen1@student.tue.nl>
2023-12-17 15:46:54 +0100

    dag 15 als oneliner

--- 15/solution.py
+++ 15/solution.py
@@ -1,12 +1,1 @@
-with open("input.txt", "r") as f:
-    input = [part.strip() for part in f.read().strip().split(',') if part.strip() != ""]
-
-def algorithm(part):
-    curr = 0
-    for char in part:
-        curr += ord(char)
-        curr *= 17
-        curr %= 256
-    return curr
-
-print(sum([algorithm(part) for part in input]))
+with open("input.txt", "r") as f: print(sum([(lambda part, acc=0: ((lambda acc: [acc := ((acc + ord(char)) * 17) % 256 for char in part])(acc))[-1])(item) for item in [part.strip() for part in f.read().strip().split(',') if part.strip()]]))
